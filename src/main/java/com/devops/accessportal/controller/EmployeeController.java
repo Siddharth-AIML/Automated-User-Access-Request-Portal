51664346eea02be53e45271079e38f15f24e202f
Siddharth Metkari <siddharthmetkari00@gmail.com>
2026-09-09 09:43:45 +0530

    Added Defect deliberately

--- src/main/java/com/devops/accessportal/controller/EmployeeController.java
+++ src/main/java/com/devops/accessportal/controller/EmployeeController.java
@@ -8,6 +8,6 @@ public class EmployeeController {
 
     @GetMapping("/employee/dashboard")
     public String employeeDashboard() {
-        return "employee-dashboard";
+        return "employee-dashboard-defect";
     }
 }
